4c63fa2a6bc817c194806beaa3c05c0e2225a4b8
Luey-Ny0ike <nyoikelewis@gmail.com>
2022-08-26 09:49:30 +0300

    not needed

---
+++ config/initializers/trello.rb
@@ -0,0 +1,8 @@
+# require 'trello'
+#
+# Trello.configure do |config|
+#   config.developer_public_key = Rails.application.credentials.dig(:trello, :application_key)
+#   config.member_token = Rails.application.credentials.dig(:trello, :member_token)
+# end
+
+# This was initially for the gem based integration of trello which I scrapped in favour of the Rest Api
