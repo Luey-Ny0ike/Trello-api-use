8a6455d60a9b03a411021de578f8b491a7b28223
Luey-Ny0ike <nyoikelewis@gmail.com>
2022-08-26 09:49:16 +0300

    configured the routes used

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get 'boards/:id', to: 'home#show', as: 'single_board'
+  post 'lists/:id/create_card', to: 'home#create_card', as: 'create_card'
 end
