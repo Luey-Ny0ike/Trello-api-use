caeb9288e81168b584f8b28aeb71261122ffdf28
Luey-Ny0ike <nyoikelewis@gmail.com>
2022-08-29 04:38:31 +0300

    Added extra routes for the new card methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'boards/:id', to: 'home#show', as: 'single_board'
   post 'lists/:id/create_card', to: 'home#create_card', as: 'create_card'
+  delete 'cards/:id', to: 'home#delete_card', as: 'delete_card'
+  patch 'cards/:id', to: 'home#update_card', as: 'update_card'
 end
